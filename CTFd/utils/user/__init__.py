2a1dd69d2c1baabd5c5df19025ac406caf9db52d
Jan Zmeskal <jzmeskal@redhat.com>
2020-01-15 16:36:08 +0100

    Display user points

--- CTFd/utils/user/__init__.py
+++ CTFd/utils/user/__init__.py
@@ -14,6 +14,10 @@ def get_current_user():
         return None
 
 
+def get_current_user_score():
+    return get_current_user().score
+
+
 def get_current_team():
     if authed():
         user = get_current_user()
